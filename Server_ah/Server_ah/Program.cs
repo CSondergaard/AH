091007c9db2c5852d27a95033b1fcd37a54e4611
Casper Søndergaard <casp4807@hotmail.com>
2017-08-18 09:14:29 +0200

    project

---
+++ Server_ah/Server_ah/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_ah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+            p.run();
+
+        }
+
+        private void run()
+        {
+
+            Console.WriteLine("yo bitheces");
+            string pass = Console.ReadLine();
+            string salt = login.GetRandomSalt();
+            string nytpas = login.HashString(pass, salt);
+
+            Console.WriteLine("Din salt er: " + salt);
+            Console.WriteLine(login.HashString(pass, salt));
+            Console.WriteLine(login.HashString(pass, salt));
+            Console.WriteLine(login.HashString(pass, salt));
+            Console.WriteLine(login.HashString(pass, salt));
+
+            Console.WriteLine(nytpas);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
